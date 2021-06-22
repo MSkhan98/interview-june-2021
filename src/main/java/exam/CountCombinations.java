33d2f44ef6e4e63a58b369e43d9d8ff31cb43d39
MSkhan98 <69440407+MSkhan98@users.noreply.github.com>
2021-06-22 12:18:28 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -3,7 +3,25 @@ package exam;
 public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+ for(int i = 0; i < k; i++){  
+            int j, last;  
+       
+            last = arr[arr.length-1];  
+          
+            for(j = arr.length-1; j > 0; j--){  
+                //Shift element of array by one  
+                arr[j] = arr[j-1];  
+            }  
+    
+            arr[0] = last;  
+        }  
+      
+        System.out.println();  
+          
+         
+        System.out.println("Array after right rotation: ");  
+        for(int i = 0; i< arr.length; i++){  
+            System.out.print(arr[i] + " ");  
     return 0;
   }
 
